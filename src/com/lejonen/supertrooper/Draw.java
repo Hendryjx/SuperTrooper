adcea38ef77f5f99755fc05bc0851d579299cbc0
Hanna Kalholm <hannakalholm@gmail.com>
2018-02-28 12:15:15 +0100

    Rename the class Draw.

---
+++ src/com/lejonen/supertrooper/Draw.java
@@ -0,0 +1,22 @@
+package com.lejonen.supertrooper;
+
+public class Draw {
+
+    public void drawPlayer(){
+        //Skicka in terminal, den som skall anropa draw måste ha tillgång till terminalen redan.
+
+        //Skicka in positioner för Player, Shot, Creature, Collision(?).
+    }
+
+    public void drawShot(){
+        //Skicka in terminal, den som skall anropa draw måste ha tillgång till terminalen redan.
+
+        //Skicka in positioner för Player, Shot, Creature, Collision(?).
+    }
+
+    public void drawCreature(){
+        //Skicka in terminal, den som skall anropa draw måste ha tillgång till terminalen redan.
+
+        //Skicka in positioner för Player, Shot, Creature, Collision(?).
+    }
+}
