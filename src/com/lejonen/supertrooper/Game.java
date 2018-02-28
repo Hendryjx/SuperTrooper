006ee602a8337e40b9e2ed5b9aa2f7147ae3003b
Erik Lindeborg <erik.lindeborg@gmail.com>
2018-02-28 14:20:50 +0100

    Game loop implemented

--- src/com/lejonen/supertrooper/Game.java
+++ src/com/lejonen/supertrooper/Game.java
@@ -2,7 +2,75 @@ package com.lejonen.supertrooper;
 
 public class Game {
 
+    public boolean running;
+
     //Game initieras.
+
+
+
+    public Game() {
+
+        this.running = true;
+
+
+
+    }
+
+
+    //Start game.
+
+    public void startGame() {
+
+
+//        boolean RENDER_TIME = true;
+        long initialTime = System.nanoTime();
+        final double timeU = 1000000000 / 60; //Updates per second
+        final double timeF = 1000000000 / 30; //FPS;
+        double deltaU = 0, deltaF = 0;
+        int frames = 0, ticks = 0;
+        long timer = System.currentTimeMillis();
+
+        while (this.running) {
+
+            long currentTime = System.nanoTime();
+            deltaU += (currentTime - initialTime) / timeU;
+            deltaF += (currentTime - initialTime) / timeF;
+            initialTime = currentTime;
+
+            if (deltaU >= 1) {
+                update();
+//                ticks++;
+                deltaU--;
+            }
+
+            if (deltaF >= 1) {
+                render();
+//                frames++;
+                deltaF--;
+            }
+
+//            if (System.currentTimeMillis() - timer > 1000) {
+//                if (RENDER_TIME) {
+//                    System.out.println(String.format("UPS: %s, FPS: %s", ticks, frames));
+//                }
+//                frames = 0;
+//                ticks = 0;
+//                timer += 1000;
+            }
+        }
+
+
+        public void render(){
+
+            Draw.drawPlayer(player);
+            Draw.drawCreature();
+            Draw.drawShot();
+
+
+        }
+
+    }
+
     //Input behöver hanteras.
     //gameLoop --> handleInput, updateObjects, draw.
     //Kontroll om gameLoopen skall köras igen.
